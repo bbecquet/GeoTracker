a5f874958bcce568712ad04adcaf482ab7618e80
bbecquet <benjamin.becquet@gmail.com>
2016-10-28 17:47:59 +0200

    Add duration in track stats

--- src/components/TrackStats.js
+++ src/components/TrackStats.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { getLength } from '../models/trackUtils';
 import Length from './Length';
 import { getSetting } from '../models/settings';
+import moment from 'moment';
+import _ from 'lodash';
 
 class Track extends Component {
     static propTypes = {
@@ -11,14 +13,16 @@ class Track extends Component {
 
     render() {
         const { positions } = this.props;
+        const duration = _.last(positions).timestamp - _.first(positions).timestamp;
 
         return (
             <div className="trackStats">
                 <p>{`${positions.length} points`}</p>
-                <p>Distance <Length
+                <p>Distance: <Length
                     meters={getLength(positions)}
                     imperialSystem={getSetting('lengthUnit') === 'imperial'}
                 /></p>
+                <p>Duration: {moment.duration(duration).humanize()}</p>
             </div>
         );
     }
