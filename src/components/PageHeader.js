acc73df6b5abd72ae0a7d4ea9478fdcdb3210cc7
Benjamin Becquet <benjamin.becquet@gmail.com>
2016-10-30 21:39:39 +0100

    Title, logo, favicon and stuff

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import logo from '../imgs/logo.png';
 
 class PageHeader extends Component {
     static propTypes = {
@@ -11,7 +12,10 @@ class PageHeader extends Component {
     render() {
         return <header className="pageHeader">
             <div className="pageHeader-left">
-                {this.props.backPath && <Link to={this.props.backPath}>❮</Link>}
+                {this.props.backPath
+                    ? <Link to={this.props.backPath}>❮</Link>
+                    : <img src={logo} alt="" />
+                }
             </div>
             <h1>{this.props.title}</h1>
             <div className="pageHeader-right">
