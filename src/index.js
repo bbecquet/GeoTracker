0d678326575a1eb76bef2094e037b69779ad25c3
bbecquet <benjamin.becquet@gmail.com>
2016-11-03 17:42:56 +0100

    Redirect non-matching urls to root

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { Router, Route, IndexRedirect, Redirect, browserHistory } from 'react-router';
 import TrackList from './pages/TrackList.js';
 import SingleTrack from './pages/SingleTrack.js';
 import TrackingView from './pages/TrackingView.js';
@@ -23,8 +23,8 @@ ReactDOM.render(
             <Route path="tracks/:trackId" component={SingleTrack}></Route>
             <Route path="tracks/:trackId/tracking" component={TrackingView}></Route>
             <Route path="settings" component={SettingsView}/>
+            <Redirect path="*" to="/"/>
         </Route>
     </Router>,
-    // <Route path="*" component={NoMatch}/>, redirect to home
     document.getElementById('root')
 );
