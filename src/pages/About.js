bff1f1077b99dd0eb2f1954710492df2c2f363e5
bbecquet <benjamin.becquet@gmail.com>
2019-09-10 15:34:34 +0200

    Update copyright date to 2019

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ class About extends Component {
                     <h1>GeoTracker</h1>
                     <img src={logo} alt=""/>
                     <p><a className="link" href="https://github.com/bbecquet/GeoTracker">GitHub project page</a>.</p>
-                    <p>© 2016 <a className="link" href="http://bbecquet.net">Benjamin Becquet</a>.</p>
+                    <p>© 2019 <a className="link" href="http://bbecquet.net">Benjamin Becquet</a>.</p>
                 </div>
                 <h2>Attributions</h2>
                 <p>Map views powered by <a className="link" href="http://leafletjs.com">Leaflet</a>.</p>
